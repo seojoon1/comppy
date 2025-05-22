0fe3a2538b22aaf05d4565e5712760f79385c480
comppy <kim1122aass@gmail.com>
2025-05-22 22:28:05 +0900

    feat/메인화면 에니메이션 추가

--- abgl/wwe/js/mainWeb.js
+++ abgl/wwe/js/mainWeb.js
@@ -1,17 +1,22 @@
-// const dnm = document.querySelector(`#DnM`);
-document.addEventListener(`DOMContentLoaded`, function(){ //DOMContentLoaded = html 의 모든 요소를 읽고 DOM 을 완성했을 때 실행하게 해줌
-    const DnM = document.querySelector(`#DnM`);
-    DnM.addEventListener('click', function(event){ //클릭시 함수실행
-        const userInput = prompt(`관리자 ID를 입력하세요`); //관리자 id 프롬포트로 받음
-        if(userInput !== `comppy`){
-            event.preventDefault(); //이 조건이 실행되면 링크 실행 안됨
-            alert('다시 입력 하세요.');
+    // const dnm = document.querySelector(`#DnM`);
+    document.addEventListener(`DOMContentLoaded`, function(){ //DOMContentLoaded = html 의 모든 요소를 읽고 DOM 을 완성했을 때 실행하게 해줌
+        const DnM = document.querySelector("#DBnMD");
+        if(DnM){
+            DnM.addEventListener("click", function(event){ //클릭시 함수실행
+                const userInput = prompt(`관리자 ID를 입력하세요`); //관리자 id 프롬포트로 받음
+                if(userInput !== `comppy`){
+                    event.preventDefault(); //이 조건이 실행되면 링크 실행 안됨
+                    alert('다시 입력 하세요.');
+                }
+                else{
+                    alert('확인.');
+                }
+            })
         }
         else{
-            alert('확인.');
+            alert("꺼져")
         }
     })
-})
-// dnm.addEventListener(`mousedown`,function(){
-//     alert(`개발중 입니다.`)
-// })
+    // dnm.addEventListener(`mousedown`,function(){
+    //     alert(`개발중 입니다.`)
+    // })
