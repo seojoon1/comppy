e9fc1dc9dfa2d22ded17961319a9316c9027f645
comppy <kim1122aass@gmail.com>
2025-05-26 10:06:55 +0900

    test

--- abgl/wwe/ㅇㅈ/js공부용webapi/jsdataset.js
+++ abgl/wwe/ㅇㅈ/js공부용webapi/jsdataset.js
@@ -1,6 +1,7 @@
 let img = document.querySelector('img'); //img 태그 가져오고
-let liList = document.querySelectorAll("li");//li도 가져오고 
+let vid = document.querySelector('video');
 let sitem = document.querySelector(".sitem")
+let liList = document.querySelectorAll("li");//li도 가져오고 
 
 liList[0].addEventListener('click',imgevent);
 liList[1].addEventListener('click',imgevent);
@@ -13,5 +14,7 @@ liList[3].addEventListener('click',imgevent);
 
 function imgevent(event){ //클릭하면 값을 변경하는 함수 만들고
     img.setAttribute("src", event.target.dataset.img);
+    vid.setAttribute("src", event.target.dataset.video);
     sitem.textContent = event.target.textContent;
+
 }
